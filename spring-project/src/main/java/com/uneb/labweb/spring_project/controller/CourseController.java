26e96ece24f576c7e3fe27aebf5a5f658bac625d
IKEDA\duike <eduardoikeda@outlook.com.br>
2024-09-16 12:13:18 -0300

    no message

--- spring-project/src/main/java/com/uneb/labweb/spring_project/controller/CourseController.java
+++ spring-project/src/main/java/com/uneb/labweb/spring_project/controller/CourseController.java
@@ -2,14 +2,19 @@ package com.uneb.labweb.spring_project.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uneb.labweb.spring_project.model.Course;
 import com.uneb.labweb.spring_project.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -22,4 +27,11 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus (code = HttpStatus.ACCEPTED)
+    public Course create(@RequestBody Course course) {       
+        return courseRepository.save(course);
+    }
+    
 }
