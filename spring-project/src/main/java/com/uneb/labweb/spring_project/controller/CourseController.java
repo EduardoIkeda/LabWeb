4ed46537d82cc05404f54c101e05c2725b49079b
IKEDA\duike <eduardoikeda@outlook.com.br>
2024-09-21 10:39:28 -0300

    [ADD] adicionado novo projeto angular para pratica das atividades

--- spring-project/src/main/java/com/uneb/labweb/spring_project/controller/CourseController.java
+++ spring-project/src/main/java/com/uneb/labweb/spring_project/controller/CourseController.java
@@ -3,8 +3,11 @@ package com.uneb.labweb.spring_project.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,9 +32,27 @@ public class CourseController {
     }
 
     @PostMapping
-    @ResponseStatus (code = HttpStatus.ACCEPTED)
-    public Course create(@RequestBody Course course) {       
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getMethodName(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
     
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(record -> {
+                    record.setName(course.getName());
+                    record.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
